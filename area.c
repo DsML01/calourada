a9bacfcf8fe4bc20257ffd36f1fe028e519ecd0a
DsML01 <davimelovas61@gmail.com>
2023-06-21 11:10:03 -0300

    Add exercicio de area

---
+++ area.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+ float A;
+ float B;
+ float C;
+ float pi = 3.14159;
+
+  scanf("%f %f %f", &A, &B, &C);
+
+  printf ("Triangulo %.3f\n", ( (A*C)/2 ));
+  printf ("Circulo %.3f\n", (pi*(C*C)));
+  printf ("Trapezio %.3f\n", (((A+B)*C)/2));
+  printf ("Quadrado %.3f\n", (B*B));
+  printf ("Retângulo %.3f\n", (A*B));
+}
+
+
