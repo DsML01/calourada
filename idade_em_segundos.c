8e7d2bce724457f2f56f6d628e06b3cb3934b170
DsML01 <davimelovas61@gmail.com>
2023-06-21 11:13:53 -0300

    Add idade para segundos

---
+++ idade_em_segundos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main (){
+  int P1, P2, P3;
+   scanf("%d%d%d", &P1, &P2, &P3);
+    printf("PESSOA 1 EM SEGUNDOS = %d\n", (P1*365*24*60*60));
+    printf("PESSOA 2 EM SEGUNDOS = %d\n", (P2*365*24*60*60));
+    printf("PESSOA 3 EM SEGUNDOS = %d\n", (P3*365*24*60*60));
+ return 0;
+ }
